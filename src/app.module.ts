2d559bea369a1d72c1e1929be40d126bb641aea9
eunkangchoi <ekk12mv2@gmail.com>
2022-10-07 00:32:08 +0900

    Add: Add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
+  imports: [MoviesModule],
   controllers: [MoviesController], // 라우터에서 호출한 url을 가져오고 처리 function을 실행하는 파일
   providers: [MoviesService], //
 })
