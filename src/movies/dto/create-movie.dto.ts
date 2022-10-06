cb404f7c7ab8e20bda501b2d1886aef3ce4ea28c
eunkangchoi <ekk12mv2@gmail.com>
2022-10-07 00:22:30 +0900

    Add update-movie validation

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,5 +1,5 @@
 // class-validator: 클래스 타입의 유효성을 검사.
-import { IsString, IsNumber } from 'class-validator';
+import { IsString, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateMovieDto {
   @IsString()
@@ -8,6 +8,7 @@ export class CreateMovieDto {
   @IsNumber()
   readonly year: number;
 
+  @IsOptional()
   @IsString({ each: true })
   readonly genres: string[];
 }
