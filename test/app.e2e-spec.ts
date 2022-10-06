08836b03db78a5653229cc0994a8a28519001c56
eunkangchoi <ekk12mv2@gmail.com>
2022-10-07 02:01:07 +0900

    :test_tube: Add e2e Test cases

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,31 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my movie API');
+  });
+
+  // create
+  describe('/movies', () => {
+    // getAll
+    it('/movies (GET)', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+
+    // create movie
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'test',
+          year: 2013,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+
+    // delete movie
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
   });
 });
